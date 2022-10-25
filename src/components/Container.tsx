cff06d7d2373195d5e188564e430946bcb983e6b
Mehrdad Shokri <code.poet95@gmail.com>
2022-10-25 14:17:04 +0330

    trying to fix an infinite loop

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -139,7 +139,7 @@ export default function () {
             <Story
                 action={toggleState}
                 bufferAction={bufferAction}
-                playState={globalPause}
+                playState={pause}
                 story={stories[currentId]}
                 getVideoDuration={getVideoDuration}
             />
